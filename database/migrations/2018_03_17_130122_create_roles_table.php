aad6c103268aa81731557a1286e3bf13ddaff370
Igor5005 <igor5005@gmail.com>
2020-04-23 18:49:15 +0300

    23.04.2020

---
+++ database/migrations/2018_03_17_130122_create_roles_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRolesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('roles', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->bigIncrements('id');
+            $table->string('name')->unique();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::statement('SET FOREIGN_KEY_CHECKS = 0');
+        Schema::drop('roles');
+        DB::statement('SET FOREIGN_KEY_CHECKS = 1');
+    }
+}
